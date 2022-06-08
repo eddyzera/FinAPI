b68e555bd73c5f0da4e15e4b346ca6ba490f47f8
eddyzera <edgar.nascimento@newgo.com.br>
2022-06-08 17:40:00 -0300

    [feat]: should create a new user if cpf not exists

--- src/index.js
+++ src/index.js
@@ -10,13 +10,16 @@ app.use(express.json())
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body
   const id = uuidV4()
+  const customerAlreadyExiste = customers.some((customer) => customer.cpf === cpf)
+  if (customerAlreadyExiste) {
+    return response.status(400).json({ error: 'Customer already exists!' })
+  }
   customers.push({
     cpf,
     name,
     id,
     statement: []
   })
-
   return response.status(201).json(customers)
 })
 
