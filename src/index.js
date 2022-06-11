28a55b3d6b5659d6b96ad28eb4add1c01e45b615
eddyzera <edgar.nascimento@newgo.com.br>
2022-06-11 01:10:06 -0300

    [feat]: created middleware for verification if CPF is valid

--- src/index.js
+++ src/index.js
@@ -6,6 +6,19 @@ const app = express()
 app.use(express.json())
 const customers = []
 
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers
+  const customer = customers.find((customer) => customer.cpf === parseInt(cpf))
+  if(!customer) {
+    return response.status(400).json({ message: "Customer not found" })
+  }
+  /**
+   *  meu request agora guarda o custumer
+   */
+  request.customer = customer
+  return next()
+}
+
 app.post('/account', (request, response) => {
   const { cpf, name } = request.body
   const customerAlreadyExists = customers.some((customer) => customer.cpf === parseInt(cpf))
@@ -21,10 +34,10 @@ app.post('/account', (request, response) => {
   return response.status(201).json(customers)
 })
 
-app.get('/statement/:cpf', (request, response) => {
-  const { cpf } = request.params
-  const customer = customers.find((customer) => customer.cpf === parseInt(cpf))
+app.get('/statement', verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request
   return response.status(200).json(customer.statement)
 })
 
-app.listen('3002')
+
+app.listen('3000')
